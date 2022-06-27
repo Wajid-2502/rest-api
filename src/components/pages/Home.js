ad9beb49a931dd6e58906a1b6d2bd5e0f5b2eed9
wajid <wajid@chopdawg.com>
2022-06-27 18:03:40 +0500

    Home Screen updated

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React from "react";
+import { createStyles, Grid, Typography } from "@mui/material";
+import { deepPurple } from "@mui/material/colors";
+import { Box } from "@mui/system";
+
+const useStyle = createStyles({
+  headingColor: {
+    backgroundColor: deepPurple[400],
+  },
+});
 
 export const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Box
+        textAlign={"center"}
+        sx={{ background: "#CCC3E5", padding: "10px 0px" }}
+      >
+        <Typography variant="h2">CRUD API</Typography>
+      </Box>
+      <Grid container>
+        <Grid item md={6} sm={12} xs={12}>
+          1
+        </Grid>
+        <Grid item md={6} sm={12} xs={12}>
+          2
+        </Grid>
+      </Grid>
+    </>
   );
-}
- export default Home;
+};
+export default Home;
